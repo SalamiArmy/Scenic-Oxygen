b9761a03af0f7e235f2a5ccb433aadf9d3ff9832
SalamiArmy <ultrakiff@gmail.com>
2016-06-03 20:34:22 +0200

    retry_on_telegram_error retries on all errors

--- commands/retry_on_telegram_error.py
+++ commands/retry_on_telegram_error.py
@@ -8,11 +8,9 @@ def SendDocumentWithRetry(bot, chat_id, imagelink, requestText):
     sendException = True
     while sendException and numberOfRetries > 0:
         try:
-            bot.sendDocument(chat_id=chat_id,
-                             filename=requestText.encode('utf-8'),
-                             document=imagelink.encode('utf-8'))
+            bot.sendDocument(chat_id=chat_id, filename=requestText.encode('utf-8'), document=imagelink.encode('utf-8'))
             sendException = False
-        except telegram.TelegramError:
+        except:
             sendException = True
             numberOfRetries -= 1
     return numberOfRetries > 0
@@ -27,7 +25,7 @@ def SendPhotoWithRetry(bot, chat_id, imagelink, captionText, user):
                           caption=(user + ': ' if not user == '' else '') + captionText.encode('utf-8') +
                                   (' ' + imagelink if len(imagelink) < 100 else '').encode('utf-8'))
             sendException = False
-        except telegram.TelegramError:
+        except:
             sendException = True
             numberOfRetries -= 1
     return numberOfRetries > 0
