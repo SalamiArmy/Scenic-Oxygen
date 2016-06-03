a90aade019b5795b8cf087b835ce5e49e593b0d9
SalamiArmy <ultrakiff@gmail.com>
2016-06-03 20:01:36 +0200

    remove try catch from /get

--- commands/get.py
+++ commands/get.py
@@ -12,48 +12,33 @@ from commands import retry_on_telegram_error
 
 def run(bot, keyConfig, chat_id, user, message):
     requestText = message.replace(bot.name, "").strip()
-
-    # Ashley: Added a try catch here-
-    # For weird 'Unautherized' error when sending photos.
-    # Keeps track of the last user to receive a chat action.
-    # Satisfies pending chat actions with a message instead of a photo.
-    try:
-        googurl = 'https://www.googleapis.com/customsearch/v1'
-        args = {'cx': keyConfig.get('Google', 'GCSE_SE_ID'),
-                'key': keyConfig.get('Google', 'GCSE_APP_ID'),
-                'searchType': "image",
-                'safe': "off",
-                'q': requestText}
-        realUrl = googurl + '?' + urllib.urlencode(args)
-        data = json.load(urllib.urlopen(realUrl))
-        if 'items' in data and len(data['items']) >= 9:
-            thereWasAnError = True
-            imagelink = 'x-raw-image:///'
-            offset = 0
-            while thereWasAnError and offset < 10:
-                imagelink = data['items'][random.randint(0, 9) + offset]['link']
-                offset += 1
-                bot.sendChatAction(chat_id=chat_id, action=telegram.ChatAction.UPLOAD_PHOTO)
-                if not imagelink.startswith('x-raw-image:///') and imagelink != '':
-                    thereWasAnError = not retry_on_telegram_error.SendPhotoWithRetry(bot, chat_id, imagelink, requestText, user)
-                else:
-                    thereWasAnError = True
-            if thereWasAnError or not offset < 10:
-                bot.sendMessage(chat_id=chat_id, text='I\'m sorry ' + (user if not user == '' else 'Dave') +
-                                                      ', I\'m afraid I can\'t find any images for ' +
-                                                      string.capwords(requestText.encode('utf-8')))
-        else:
+    googurl = 'https://www.googleapis.com/customsearch/v1'
+    args = {'cx': keyConfig.get('Google', 'GCSE_SE_ID'),
+            'key': keyConfig.get('Google', 'GCSE_APP_ID'),
+            'searchType': "image",
+            'safe': "off",
+            'q': requestText}
+    realUrl = googurl + '?' + urllib.urlencode(args)
+    data = json.load(urllib.urlopen(realUrl))
+    if 'items' in data and len(data['items']) >= 9:
+        thereWasAnError = True
+        imagelink = 'x-raw-image:///'
+        offset = 0
+        while thereWasAnError and offset < 10:
+            imagelink = data['items'][random.randint(0, 9) + offset]['link']
+            offset += 1
+            bot.sendChatAction(chat_id=chat_id, action=telegram.ChatAction.UPLOAD_PHOTO)
+            if not imagelink.startswith('x-raw-image:///') and imagelink != '':
+                thereWasAnError = not retry_on_telegram_error.SendPhotoWithRetry(bot, chat_id, imagelink, requestText, user)
+            else:
+                thereWasAnError = True
+        if thereWasAnError or not offset < 10:
             bot.sendMessage(chat_id=chat_id, text='I\'m sorry ' + (user if not user == '' else 'Dave') +
                                                   ', I\'m afraid I can\'t find any images for ' +
                                                   string.capwords(requestText.encode('utf-8')))
-    except:
-        adminGroupId = keyConfig.get('BotAdministration', 'ADMIN_GROUP_CHAT_ID')
-        if user != adminGroupId:
-            bot.sendMessage(chat_id=chat_id, text=requestText + ': ' + imagelink)
-        if adminGroupId:
-            bot.sendMessage(chat_id=adminGroupId, text='Error: ' + str(sys.exc_info()[1]) + '\n' +
-                                                  'Request Text: ' + requestText + '\n' +
-                                                  'Url: ' + imagelink)
-
+    else:
+        bot.sendMessage(chat_id=chat_id, text='I\'m sorry ' + (user if not user == '' else 'Dave') +
+                                              ', I\'m afraid I can\'t find any images for ' +
+                                              string.capwords(requestText.encode('utf-8')))
 
 
