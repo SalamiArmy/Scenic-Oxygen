2e5d498d521f99fb459a11c734f40236fb06946b
SalamiArmy <ultrakiff@gmail.com>
2016-06-03 20:27:31 +0200

    /get and /getgif error handling updated again

--- commands/get.py
+++ commands/get.py
@@ -22,16 +22,15 @@ def run(bot, keyConfig, chat_id, user, message):
     data = json.load(urllib.urlopen(realUrl))
     if 'items' in data and len(data['items']) >= 9:
         thereWasAnError = True
-        imagelink = 'x-raw-image:///'
         offset = 0
+        randint = random.randint(0, 9)
+        bot.sendChatAction(chat_id=chat_id, action=telegram.ChatAction.UPLOAD_PHOTO)
         while thereWasAnError and offset < 10:
-            imagelink = data['items'][random.randint(0, 9) + offset]['link']
+            randint_offset = randint + offset
+            imagelink = data['items'][randint_offset if randint_offset < 10 else randint_offset - 10]['link']
             offset += 1
-            bot.sendChatAction(chat_id=chat_id, action=telegram.ChatAction.UPLOAD_PHOTO)
             if not imagelink.startswith('x-raw-image:///') and imagelink != '':
                 thereWasAnError = not retry_on_telegram_error.SendPhotoWithRetry(bot, chat_id, imagelink, requestText, user)
-            else:
-                thereWasAnError = True
         if thereWasAnError or not offset < 10:
             bot.sendMessage(chat_id=chat_id, text='I\'m sorry ' + (user if not user == '' else 'Dave') +
                                                   ', I\'m afraid I can\'t find any images for ' +
