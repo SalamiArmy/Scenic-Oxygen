24a9e836c5c2f67407197593b5ee4b499cf4150f
SalamiArmy <ultrakiff@gmail.com>
2016-06-10 23:04:20 +0200

    add optional wiki keywords

--- vocabs/intend_wiki.py
+++ vocabs/intend_wiki.py
@@ -4,4 +4,18 @@ from adapt.intent import IntentBuilder
 def generate_vocab(engine):
     engine.register_regex_entity("(what|who) is (?P<WhatWhoIs>.*)")
 
-    return IntentBuilder("ImageIntent").require('WhatWhoIs').build()
+    wiki_keywords = [
+        "know",
+        "understand",
+        "think",
+        "comprehend",
+        "knowledge"
+    ]
+
+    for wt in wiki_keywords:
+        engine.register_entity(wt, "WikiKeywords")
+
+    return IntentBuilder("ImageIntent")\
+        .require('WhatWhoIs')\
+        .optionally('WikiKeywords')\
+        .build()
