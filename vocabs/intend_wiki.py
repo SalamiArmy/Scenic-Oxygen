bfe71f6aafaf4c3e03e94b353b3fca072abf9725
SalamiArmy <ultrakiff@gmail.com>
2016-06-10 20:57:51 +0200

    wrong regex

--- vocabs/intend_wiki.py
+++ vocabs/intend_wiki.py
@@ -2,6 +2,6 @@ from adapt.intent import IntentBuilder
 
 
 def generate_vocab(engine):
-    engine.register_regex_entity("(what is (?P<WhatWhoIs>.*))|(who is (?P<WhatWhoIs>.*))")
+    engine.register_regex_entity("(what|who) is (?P<WhatWhoIs>.*))")
 
     return IntentBuilder("ImageIntent").require('WhatWhoIs').build()
