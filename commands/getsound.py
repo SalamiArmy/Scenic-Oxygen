40fbf83ab391c8d832eaabe8267e1944023e593e
SalamiArmy <ultrakiff@gmail.com>
2016-06-19 11:26:11 +0200

    fix getsound

--- commands/getsound.py
+++ commands/getsound.py
@@ -10,7 +10,7 @@ def run(bot, keyConfig, chat_id, user, message, intention_confidence=0.0):
     track = client.get('/tracks', q=requestText.encode('utf-8'), sharing='public')
     if len(track) >= 1:
         bot.sendMessage(chat_id=chat_id, text=(user + ': ' if not user == '' else '') + track[0].permalink_url +
-                        '\nMight I add that I am ' + str(intention_confidence) + '% confident you wanted to hear this.' if intention_confidence > 0.0 else '')
+                                              ('\nMight I add that I am ' + str(intention_confidence) + '% confident you wanted to hear this.' if intention_confidence > 0.0 else ''))
         return True
     else:
         if intention_confidence == 0.0:
