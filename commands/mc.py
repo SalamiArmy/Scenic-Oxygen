fdef34b00677577e388222f5d5314e1a95e4f160
Ashley Lewis <ashley.lewis@dev2.co.za>
2016-08-16 11:48:28 +0200

    add extra logging around /mc command

--- commands/mc.py
+++ commands/mc.py
@@ -6,9 +6,14 @@ from mcstatus import MinecraftServer
 def run(bot, keyConfig, chat_id, user, message):
 
     mcServer = keyConfig.get('Minecraft', 'SVR_ADDR')
+    print('got Minecraft server address as' +  mcServer)
     mcPort = int(keyConfig.get('Minecraft', 'SVR_PORT'))
+    print('got Minecraft port as' + mcPort)
     dynmapPort = keyConfig.get('Minecraft', 'DYNMAP_PORT')
+    print('got Dynmap port as' + dynmapPort)
     status = MinecraftServer(mcServer, mcPort).status()
+    print('got players online as' + status.players.online)
+    print('got ping as' + status.latency)
     bot.sendMessage(chat_id=chat_id, text=('The server at {0} has {1} players and replied in {2} ms' +
                                            ('' if dynmapPort == '' else '\nSee map: ' + mcServer + ':' + dynmapPort)) \
                     .format(mcServer + ':' + str(mcPort), status.players.online, status.latency))
