b0890c252c45efcd66945ded85692f23216cdd2f
Ashley Lewis <ashley.lewis@dev2.co.za>
2016-08-16 11:55:00 +0200

    fix error in /mc introduced with logging

--- commands/mc.py
+++ commands/mc.py
@@ -6,13 +6,13 @@ from mcstatus import MinecraftServer
 def run(bot, keyConfig, chat_id, user, message):
 
     mcServer = keyConfig.get('Minecraft', 'SVR_ADDR')
-    print('got Minecraft server address as' +  mcServer)
+    print('got Minecraft server address as ' +  mcServer)
     mcPort = int(keyConfig.get('Minecraft', 'SVR_PORT'))
-    print('got Minecraft port as' + mcPort)
+    print('got Minecraft port as ' + str(mcPort))
     dynmapPort = keyConfig.get('Minecraft', 'DYNMAP_PORT')
-    print('got Dynmap port as' + dynmapPort)
+    print('got Dynmap port as ' + dynmapPort)
     status = MinecraftServer(mcServer, mcPort).status()
-    print('got players online as' + status.players.online)
+    print('got players online as ' + status.players.online)
     print('got ping as' + status.latency)
     bot.sendMessage(chat_id=chat_id, text=('The server at {0} has {1} players and replied in {2} ms' +
                                            ('' if dynmapPort == '' else '\nSee map: ' + mcServer + ':' + dynmapPort)) \
