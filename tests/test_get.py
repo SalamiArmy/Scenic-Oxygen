bbe915f4ac30a305005248c91406e744c2e0b8a6
Ashley Lewis <ashley.lewis@dev2.co.za>
2016-08-16 10:45:05 +0200

    add dependancy for /mc command

--- tests/test_get.py
+++ tests/test_get.py
@@ -7,14 +7,14 @@ import commands.get as get
 
 class TestGet(unittest.TestCase):
     def test_get(self):
-        requestText = ' this is where my babies come from'
+        requestText = 'this is where my babies come from'
 
         keyConfig = ConfigParser.ConfigParser()
         keyConfig.read(["keys.ini", "..\keys.ini"])
         bot = telegram.Bot(keyConfig.get('Telegram', 'TELE_BOT_ID'))
-        #chatId = keyConfig.get('BotAdministration', 'ADMIN_GROUP_CHAT_ID')
+        chatId = keyConfig.get('BotAdministration', 'ADMIN_GROUP_CHAT_ID')
 
         #for bot group:
-        chatId = -130436192
+        #chatId = -130436192
 
         get.run(bot, keyConfig, chatId, 'SalamiArmy', requestText)
