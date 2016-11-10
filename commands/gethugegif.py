bfe08a035ec8084a80b161d41960d3c0c0aae08f
SalamiArmy <ultrakiff@gmail.com>
2016-11-10 19:47:36 +0200

    animated /gethugeguf

--- commands/gethugegif.py
+++ commands/gethugegif.py
@@ -3,6 +3,8 @@ import json
 import random
 import string
 import urllib
+import io
+from PIL import Image
 
 import telegram
 
@@ -10,6 +12,7 @@ from commands import retry_on_telegram_error
 
 
 def run(bot, keyConfig, chat_id, user, message):
+    global gif
     requestText = message.replace(bot.name, "").strip()
 
     googurl = 'https://www.googleapis.com/customsearch/v1?&searchType=image&num=10&safe=off&' \
@@ -17,10 +20,43 @@ def run(bot, keyConfig, chat_id, user, message):
                                                                                       'GCSE_APP_ID') + '&q='
     realUrl = googurl + requestText.encode('utf-8') + "&imgSize=xlarge" + "&fileType=gif"
     data = json.load(urllib.urlopen(realUrl))
-    if 'items' in data and len(data['items']) >= 1:
-        imagelink = data['items'][random.randint(0, 9)]['link']
+    offset = 0
+    thereWasAnError = True
+    if 'items' in data and len(data['items'])-1 >= 0:
+        randint = random.randint(0, len(data['items'])-1)
         bot.sendChatAction(chat_id=chat_id, action=telegram.ChatAction.UPLOAD_PHOTO)
-        return retry_on_telegram_error.SendDocumentWithRetry(bot, chat_id, imagelink, requestText)
+        while thereWasAnError and offset < 10:
+            randint_offset = randint + offset
+            imagelink = data['items'][randint_offset if randint_offset < 10 else randint_offset - 10]['link']
+            offset += 1
+            print("Openning url " + imagelink)
+            fd = urllib.urlopen(imagelink)
+            print("Reading gif...")
+            image_file = io.BytesIO(fd.read())
+            try:
+                print("Parsing gif...")
+                gif = Image.open(image_file)
+            except IOError:
+                gif.fp.close()
+                print("...not a gif")
+                thereWasAnError = True
+            else:
+                try:
+                    print("Checking gif for animation...")
+                    gif.seek(1)
+                except EOFError:
+                    gif.fp.close()
+                    print("...not animated")
+                    thereWasAnError = True
+                else:
+                    print("...gif is animated, confirmed!")
+                    thereWasAnError = not retry_on_telegram_error.SendDocumentWithRetry(bot, chat_id, imagelink, requestText)
+        if thereWasAnError or not offset < 10:
+            bot.sendMessage(chat_id=chat_id, text='I\'m sorry ' + (user if not user == '' else 'Dave') +
+                                                  ', I\'m afraid I can\'t find any huge gifs for ' +
+                                                  string.capwords(requestText.encode('utf-8')) + '.'.encode('utf-8'))
+        else:
+            return True
     else:
         bot.sendMessage(chat_id=chat_id, text='I\'m sorry ' + (user if not user == '' else 'Dave') + 
                                               ', I\'m afraid I can\'t find any huge gifs for ' +
