c7d10b30ba6aab24f0e3244126a1a00b6547ab28
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-02-22 13:03:10 +0200

    add set and reset commands as a proof of concept for storing basic lists in google data store

--- tests/test_reverseimage.py
+++ tests/test_reverseimage.py
@@ -7,14 +7,19 @@ import commands.reverseimage as reverseimage
 
 class TestReverseImage(unittest.TestCase):
     def test_reverseimage(self):
-        requestText = 'https://picsrch.me/w8_DKSMh'
+        requestText = 'http://cdn.skim.gs/images/v1/msi/bt7jpt3wd6djuwc4rdn1/women-banned-from-instagram-for-being-too-hairy-down-there'
 
         keyConfig = ConfigParser.ConfigParser()
         keyConfig.read(["keys.ini", "..\keys.ini"])
         bot = telegram.Bot(keyConfig.get('Telegram', 'TELE_BOT_ID'))
+
+        #for admin group
         chatId = keyConfig.get('BotAdministration', 'ADMIN_GROUP_CHAT_ID')
 
         #for bot group:
         #chatId = -130436192
+
+        #for personal chat
+        #chatId = 33166369
 
         reverseimage.run(bot, keyConfig, chatId, 'Admin', requestText)
