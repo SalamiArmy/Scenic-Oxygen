f73e3fe3689713ec4fa9d3b03f680a2fada5ea2c
Salami Army <ultrakiff@gmail.com>
2017-03-11 16:53:31 +0200

    unwatch targetting

--- commands/unwatch.py
+++ commands/unwatch.py
@@ -3,4 +3,4 @@
 from commands import watch
 
 def run(bot, keyConfig, chat_id, user, message, intention_confidence=0.0):
-    watch.setWatchValue('')
+    watch.setWatchValue(watch.getAllWatches().replace(',' + chat_id + ':' + message + ',', ',').replace(',' + chat_id + ':' + message, ''))
--- commands/unwatchgif.py
+++ commands/unwatchgif.py
@@ -3,4 +3,4 @@
 from commands import watchgif
 
 def run(bot, keyConfig, chat_id, user, message, intention_confidence=0.0):
-    watchgif.setWatchValue('')
+    watchgif.setWatchValue(watchgif.getAllWatches().replace(',' + chat_id + ':' + message + ',', ',').replace(',' + chat_id + ':' + message, ''))
