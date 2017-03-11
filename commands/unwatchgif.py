54c7f34b0b9ee8b8880c7bb0b48398b3bdaddfb7
Salami Army <ultrakiff@gmail.com>
2017-03-11 18:04:38 +0200

    add not opufn message for unwatch

--- commands/unwatchgif.py
+++ commands/unwatchgif.py
@@ -8,3 +8,5 @@ def run(bot, keyConfig, chat_id, user, message, intention_confidence=0.0):
     if ',' + watchedCommandName + ':' + str(chat_id) + ':' + message + ',' in watches or ',' + watchedCommandName + ':' + str(chat_id) + ':' + message in watches:
         main.removeFromAllWatches(watchedCommandName + ':' + str(chat_id) + ':' + message)
         bot.sendMessage(chat_id=chat_id, text='Watch for /' + watchedCommandName + ' ' + message + ' has been removed.')
+    else:
+        bot.sendMessage(chat_id=chat_id, text='Watch for /' + watchedCommandName + ' ' + message + ' not found.')
