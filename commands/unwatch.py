fb251a1b82aab11bc284ae8787e4f6c8baded265
Salami Army <ultrakiff@gmail.com>
2017-03-11 17:15:59 +0200

    fix my type exception

--- commands/unwatch.py
+++ commands/unwatch.py
@@ -6,5 +6,5 @@ watchedCommandName = 'get'
 def run(bot, keyConfig, chat_id, user, message, intention_confidence=0.0):
     watches = main.getAllWatches()
     if ',' + chat_id + ':' + message + ',' in watches or ',' + chat_id + ':' + message in watches:
-        main.removeFromAllWatches(chat_id + ':' + message)
+        main.removeFromAllWatches(str(chat_id) + ':' + message)
         bot.sendMessage(chat_id=chat_id, text='Watch for /' + watchedCommandName + ' ' + message + ' has been removed.')
