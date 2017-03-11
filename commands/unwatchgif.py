028e1cf02d79f2065ed82bd03a8737bb4af6c58d
Salami Army <ultrakiff@gmail.com>
2017-03-11 17:21:24 +0200

    fix unwatch using the wrong key

--- commands/unwatchgif.py
+++ commands/unwatchgif.py
@@ -5,6 +5,6 @@ watchedCommandName = 'getgif'
 
 def run(bot, keyConfig, chat_id, user, message, intention_confidence=0.0):
     watches = main.getAllWatches()
-    if ',' + str(chat_id) + ':' + message + ',' in watches or ',' + str(chat_id) + ':' + message in watches:
+    if ',' + watchedCommandName + ':' + str(chat_id) + ':' + message + ',' in watches or ',' + watchedCommandName + ':' + str(chat_id) + ':' + message in watches:
         main.removeFromAllWatches(str(chat_id) + ':' + message)
         bot.sendMessage(chat_id=chat_id, text='Watch for /' + watchedCommandName + ' ' + message + ' has been removed.')
