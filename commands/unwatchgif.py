1892f220a2d8f57bf8b02ae3d94f446bc19b959b
Salami Army <ultrakiff@gmail.com>
2017-03-12 20:08:03 +0200

    improved unwatchgif

--- commands/unwatchgif.py
+++ commands/unwatchgif.py
@@ -8,7 +8,8 @@ def run(bot, keyConfig, chat_id, user, message, intention_confidence=0.0):
     watches = main.getAllWatches()
     if ',' + watchedCommandName + ':' + str(chat_id) + ':' + message + ',' in watches or ',' + watchedCommandName + ':' + str(chat_id) + ':' + message in watches:
         main.removeFromAllWatches(watchedCommandName + ':' + str(chat_id) + ':' + message)
-        watchgif.setWatchValue(chat_id, message, '')
         bot.sendMessage(chat_id=chat_id, text='Watch for /' + watchedCommandName + ' ' + message + ' has been removed.')
     else:
         bot.sendMessage(chat_id=chat_id, text='Watch for /' + watchedCommandName + ' ' + message + ' not found.')
+    if watchgif.getWatchValue(chat_id, message) != '':
+        watchgif.setWatchValue(chat_id, message, '')
