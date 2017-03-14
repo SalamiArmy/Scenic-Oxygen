11b148b733fd99123c602a8a8213bd3956c764b0
Salami Army <ultrakiff@gmail.com>
2017-03-14 19:07:20 +0200

    update unwatchbitcoin to ignore request text

--- commands/unwatchbitcoin.py
+++ commands/unwatchbitcoin.py
@@ -11,5 +11,5 @@ def run(bot, keyConfig, chat_id, user, message, intention_confidence=0.0):
         bot.sendMessage(chat_id=chat_id, text='Watch for /' + watchedCommandName + ' ' + message + ' has been removed.')
     else:
         bot.sendMessage(chat_id=chat_id, text='Watch for /' + watchedCommandName + ' ' + message + ' not found.')
-    if watchbitcoin.getWatchValue(chat_id, message) != '':
-        watchbitcoin.setWatchValue(chat_id, message, '')
+    if watchbitcoin.getWatchValue(chat_id) != '':
+        watchbitcoin.setWatchValue(chat_id, '')
