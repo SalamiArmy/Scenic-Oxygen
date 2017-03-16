425a3da6045eb8b2c19482ae9ff5bd81d77c41fe
Salami Army <ultrakiff@gmail.com>
2017-03-16 20:41:15 +0200

    add change by amount to watchbitcoin when not talking directly to a user

--- commands/watchbitcoin.py
+++ commands/watchbitcoin.py
@@ -16,25 +16,26 @@ class WatchValue(ndb.Model):
 
 # ================================
 
-def setWatchValue(chat_id, NewValue):
+def setWatchValue(chat_id, request_text, NewValue):
     es = WatchValue.get_or_insert(watchedCommandName + ':' + str(chat_id))
-    es.currentValue = NewValue
+    es.currentValue = NewValue + (':' + request_text if request_text != '' else '')
     es.put()
 
 
-def getWatchValue(chat_id):
+def getWatchValue(chat_id, request_text):
     es = WatchValue.get_by_id(watchedCommandName + ':' + str(chat_id))
     if es:
-        return es.currentValue
+        return es.currentValue + (':' + request_text if request_text != '' else '')
     return ''
 
 
 def run(bot, keyConfig, chat_id, user, message, intention_confidence=0.0):
     priceGB, priceUS, priceZA, updateTime = bitcoin.get_bitcoin_prices()
     if priceZA:
-        OldValue = getWatchValue(chat_id)
-        if OldValue != priceZA:
-            setWatchValue(chat_id, priceZA)
+        OldValue = getWatchValue(chat_id, message)
+        price_diff = float(priceZA.replace(',', '')) - float(OldValue.replace(',', ''))
+        if OldValue != (priceZA + (':' + message if message != '' else '')):
+            setWatchValue(chat_id, message, priceZA)
             if user != 'Watcher':
                 if OldValue == '':
                     bot.sendMessage(chat_id=chat_id,
@@ -43,12 +44,13 @@ def run(bot, keyConfig, chat_id, user, message, intention_confidence=0.0):
                                          ' GBP\n' + priceZA + ' ZAR' + '\n\nTime Updated: ' + updateTime)
                 else:
                     bot.sendMessage(chat_id=chat_id,
-                                    text='Watch for /' + watchedCommandName + ' has changed by ' + str(float(priceZA.replace(',','')) - float(OldValue.replace(',',''))) + ' ZAR:\n' +
+                                    text='Watch for /' + watchedCommandName + ' has changed by ' + str(
+                                        price_diff) + ' ZAR:\n' +
                                          'The Current Price of 1 Bitcoin:\n\n' + priceUS + ' USD\n' + priceGB +
                                          ' GBP\n' + priceZA + ' ZAR' + '\n\nTime Updated: ' + updateTime)
             else:
                 bot.sendMessage(chat_id=chat_id,
-                                text='Watched /' + watchedCommandName + ' changed by ' + str(float(priceZA.replace(',','')) - float(OldValue.replace(',',''))) + ' ZAR:\n' +
+                                text='Watched /' + watchedCommandName + ' changed by ' + str(price_diff) + ' ZAR:\n' +
                                      'The New Current Price of 1 Bitcoin:\n\n' + priceUS + ' USD\n' + priceGB +
                                      ' GBP\n' + priceZA + ' ZAR' + '\n\nTime Updated: ' + updateTime)
         else:
@@ -57,8 +59,8 @@ def run(bot, keyConfig, chat_id, user, message, intention_confidence=0.0):
                                 text='Watch for /' + watchedCommandName + ' has not changed:\n' +
                                      'The Current Price of 1 Bitcoin:\n\n' + priceUS + ' USD\n' + priceGB +
                                      ' GBP\n' + priceZA + ' ZAR' + '\n\nTime Updated: ' + updateTime)
-        if not main.AllWatchesContains(watchedCommandName, chat_id):
-            main.addToAllWatches(watchedCommandName, chat_id)
+        if not main.AllWatchesContains(watchedCommandName, chat_id, message):
+            main.addToAllWatches(watchedCommandName, chat_id, message)
     else:
         bot.sendMessage(chat_id=chat_id, text='I\'m sorry ' + (user if not user == '' else 'Dave') +
                                               ', I\'m afraid I can\'t watch ' +
