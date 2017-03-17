3f255177147f9553676bf8ba5aaed805ed009dca
Salami Army <ultrakiff@gmail.com>
2017-03-17 18:44:27 +0200

    /gettopgames resolves steam game names

--- commands/gettopgames.py
+++ commands/gettopgames.py
@@ -1,10 +1,11 @@
 # coding=utf-8
 import urllib
+import urllib2
 
 from bs4 import BeautifulSoup
 
 
-def run(bot, chat_id, user):
+def run(bot, chat_id, user, message=''):
     gameResults = get_steam_top_games()
     if gameResults:
         bot.sendMessage(chat_id=chat_id, text=gameResults,
@@ -25,10 +26,26 @@ def get_steam_top_games():
         if 'data-ds-bundleid' in resultRow.attrs:
             resultList.append(resultRow['data-ds-bundleid'])
     resultsListLength = len(resultList)
-    hot_games = []
+    hot_games = '*Hot Games*'
     if resultsListLength > 0:
         SearchResultsInterator = 0
         while (SearchResultsInterator<resultsListLength):
-            hot_games += resultList[SearchResultsInterator]
+            steamGameLink = 'http://store.steampowered.com/app/' + resultList[SearchResultsInterator]
+            bypassAgeGate = urllib2.build_opener()
+            bypassAgeGate.addheaders.append(('Cookie', 'birthtime=578390401'))
+            code = bypassAgeGate.open(steamGameLink).read()
+            game_name = steam_game_name_parser(code, steamGameLink)
+            if game_name:
+                hot_games += '\n' + game_name
             SearchResultsInterator += 1
     return hot_games
+
+def steam_game_name_parser(code, link):
+    soup = BeautifulSoup(code, 'html.parser')
+
+    titleDiv = soup.find('div', attrs={'class':'apphub_AppName'})
+    if titleDiv:
+        gameTitle = titleDiv.string
+        return gameTitle
+    else:
+        return ''
