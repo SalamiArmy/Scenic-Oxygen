24f5aec7cf9e1ef0bf8de038b65c7ab80b94f290
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-04-03 15:29:38 +0200

    add new new gifs to watch gif

--- commands/watchgif.py
+++ commands/watchgif.py
@@ -13,6 +13,7 @@ watchedCommandName = 'getgif'
 class WatchValue(ndb.Model):
     # key name: str(chat_id)
     currentValue = ndb.StringProperty(indexed=False, default='')
+    allPreviousAddedLinks = ndb.StringProperty(indexed=False, default='')
 
 
 # ================================
@@ -22,12 +23,46 @@ def setWatchValue(chat_id, request, NewValue):
     es.currentValue = NewValue
     es.put()
 
+def addPreviouslyAddedLinkValue(chat_id, NewValue):
+    es = WatchValue.get_or_insert(watchedCommandName + ':' + str(chat_id))
+    if (es.allPreviousAddedLinks != ''):
+        es.allPreviousAddedLinks += '\n' + NewValue
+    else:
+        es.allPreviousAddedLinks += NewValue
+    es.put()
+
 
 def getWatchValue(chat_id, request):
     es = WatchValue.get_by_id(watchedCommandName + ':' + str(chat_id) + ':' + request)
     if es:
         return es.currentValue
     return ''
+
+def getPreviouslyAddedLinksValue(chat_id):
+    es = WatchValue.get_by_id(watchedCommandName + ':' + str(chat_id))
+    if es:
+        return es.allPreviousAddedLinks.encode('utf-8')
+    return ''
+
+def wasPreviouslyAddedLink(chat_id, gif_link):
+    allPreviousLinks = getPreviouslyAddedLinksValue(chat_id)
+    if '\n' + gif_link + '\n' in allPreviousLinks or \
+        allPreviousLinks.startswith(gif_link + '\n') or  \
+        allPreviousLinks.endswith('\n' + gif_link) or  \
+        allPreviousLinks == gif_link:
+        return True;
+    return False;
+
+def get_add_removed_links(chat_id, new_list, old_list):
+    added_games = ''
+    newly_added_games = ''
+    for item in new_list.split('\n'):
+        if item not in old_list:
+            added_games += '\n' + item
+            if not wasPreviouslyAddedLink(chat_id, item):
+                addPreviouslyAddedLinkValue(item)
+                newly_added_games += '\n' + item
+    return added_games, newly_added_games
 
 
 def run(bot, chat_id, user, keyConfig, message, intention_confidence=0.0):
@@ -38,6 +73,7 @@ def run(bot, chat_id, user, keyConfig, message, intention_confidence=0.0):
         imagelinks = data['items'][0]['link']
         for link in data['items']:
             imagelinks += '\n' + link['link']
+        links_added, newly_added_links = get_add_removed_links(chat_id, imagelinks, OldValue)
         print('got image links for ' + requestText + ' as ' + imagelinks)
         count = 0
         for link in data['items']:
@@ -52,7 +88,8 @@ def run(bot, chat_id, user, keyConfig, message, intention_confidence=0.0):
                         retry_on_telegram_error.SendDocumentWithRetry(bot, chat_id, imagelink, user)
                     else:
                         bot.sendMessage(chat_id=chat_id, text='Watched /' +
-                                                              watchedCommandName + ' ' + requestText + ' changed.' +
+                                                              watchedCommandName + ' ' + requestText + ' changed' +
+                                                              (' order.' if (links_added == '' and newly_added_links == '') else '.') +
                                                               '\nThis is number ' + str(count) + ' of 10.')
                         retry_on_telegram_error.SendDocumentWithRetry(bot, chat_id, imagelink, user)
                 else:
@@ -66,6 +103,19 @@ def run(bot, chat_id, user, keyConfig, message, intention_confidence=0.0):
                     bot.sendMessage(chat_id=chat_id, text='I\'m sorry ' + (user if not user == '' else 'Dave') +
                                                           ', I\'m afraid gif is not animated.' +
                                                           '\nThis is number ' + str(count) + ' of 10.\n' + imagelink)
+        if links_added != '':
+            for link in links_added:
+                bot.sendMessage(chat_id=chat_id, text='Watched /' +
+                                                      watchedCommandName + ' ' + requestText + ' has new gifs.' +
+                                                      '\nThis is new gif number ' + str(count) + '.')
+                retry_on_telegram_error.SendDocumentWithRetry(bot, chat_id, link, user)
+        if newly_added_links != '':
+            for link in newly_added_links:
+                bot.sendMessage(chat_id=chat_id, text='Watched /' +
+                                                      watchedCommandName + ' ' + requestText + ' has new new gifs.' +
+                                                      '\nThis is new new gif number ' + str(count) + '.')
+                retry_on_telegram_error.SendDocumentWithRetry(bot, chat_id, link, user)
+
         if OldValue != imagelinks:
             print('Setting watch value to ' + imagelinks)
             setWatchValue(chat_id, requestText, imagelinks)
