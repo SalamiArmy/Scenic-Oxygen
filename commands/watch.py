4dfa5dc3a12af3a1dbf599d996cb5253433f8839
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-04-03 15:46:57 +0200

    limit watches to 5 gifs

--- commands/watch.py
+++ commands/watch.py
@@ -73,12 +73,12 @@ def run(bot, chat_id, user, keyConfig, message, intention_confidence=0.0):
     if 'items' in data and len(data['items']) >= 9:
         OldValue = getWatchValue(chat_id, requestText)
         imagelinks = data['items'][0]['link']
-        for link in data['items']:
+        for link in data['items'][:5]:
             imagelinks += '\n' + link['link']
         links_added, newly_added_links = get_add_removed_links(chat_id, imagelinks, OldValue)
         print('got image links for ' + requestText + ' as ' + imagelinks)
         count = 0
-        for link in data['items']:
+        for link in data['items'][:5]:
             imagelink = link['link']
             count += 1
             if OldValue != imagelinks:
