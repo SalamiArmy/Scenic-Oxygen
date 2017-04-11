aa3d87c8134def280e9238c17d848125de26fe0e
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-04-11 10:30:36 +0200

    watchgif wont send has not changed messages

--- commands/watchgif.py
+++ commands/watchgif.py
@@ -26,11 +26,6 @@ def run(bot, chat_id, user, keyConfig, message, intention_confidence=0.0):
                         bot.sendMessage(chat_id=chat_id, text='Watched /' +
                                                               getgif.CommandName + ' ' + requestText + ' changed.')
                         retry_on_telegram_error.SendDocumentWithRetry(bot, chat_id, imagelink, user)
-            else:
-                if user != 'Watcher' and getgif.isGifAnimated(imagelink):
-                    bot.sendMessage(chat_id=chat_id, text=user + ', watch for /' +
-                                                          getgif.CommandName + ' ' + requestText + ' has not changed.')
-                    retry_on_telegram_error.SendDocumentWithRetry(bot, chat_id, imagelink, user)
         if not main.AllWatchesContains(getgif.CommandName, chat_id, requestText):
             main.addToAllWatches(getgif.CommandName, chat_id, requestText)
     else:
