830b8380bd05c22e4079bc38140a92d6307e3ae6
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-04-11 10:54:48 +0200

    only send one now watching message

--- commands/watchgif.py
+++ commands/watchgif.py
@@ -10,6 +10,7 @@ def run(bot, chat_id, user, keyConfig, message, intention_confidence=0.0):
     data, total_results, results_this_page = getgif.search_google_for_gifs(keyConfig, requestText)
     if 'items' in data and results_this_page >= 0:
         offset_this_page = 0
+        SentNowWatchingMessage = False
         while offset_this_page < results_this_page:
             imagelink = data['items'][offset_this_page]['link']
             offset_this_page += 1
@@ -19,20 +20,28 @@ def run(bot, chat_id, user, keyConfig, message, intention_confidence=0.0):
                 getgif.addPreviouslySeenGifsValue(chat_id, imagelink)
                 if getgif.isGifAnimated(imagelink):
                     if user != 'Watcher':
-                        bot.sendMessage(chat_id=chat_id, text='Now watching /' +
-                                                              getgif.CommandName + ' ' + requestText + '.')
+                        if not SentNowWatchingMessage:
+                            bot.sendMessage(chat_id=chat_id, text='Now watching /' +
+                                                                  getgif.CommandName + ' ' + requestText + '.')
+                            SentNowWatchingMessage = True
                         retry_on_telegram_error.SendDocumentWithRetry(bot, chat_id, imagelink, user)
                     else:
                         bot.sendMessage(chat_id=chat_id, text='Watched /' +
                                                               getgif.CommandName + ' ' + requestText + ' changed.')
                         retry_on_telegram_error.SendDocumentWithRetry(bot, chat_id, imagelink, user)
+        if user != 'Watcher' and not SentNowWatchingMessage:
+            bot.sendMessage(chat_id=chat_id, text='I\'m sorry ' + (user if not user == '' else 'Dave') +
+                                                  ', I\'m afraid I can\'t watch ' +
+                                                  'because I did not find any results for /getgif ' +
+                                                  string.capwords(requestText.encode('utf-8')) +
+                                                  ' that you haven\'t already seen.')
         if not main.AllWatchesContains(getgif.CommandName, chat_id, requestText):
             main.addToAllWatches(getgif.CommandName, chat_id, requestText)
     else:
         if user != 'Watcher':
             bot.sendMessage(chat_id=chat_id, text='I\'m sorry ' + (user if not user == '' else 'Dave') +
                                                   ', I\'m afraid I can\'t watch ' +
-                                                  'because I did not find enough results for /getgif ' +
+                                                  'because I did not find any results for /getgif ' +
                                                   string.capwords(requestText.encode('utf-8')))
 
 
