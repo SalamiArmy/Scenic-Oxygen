0050f4cc792499ebbc96b4f9df1a94c1b97c98a2
Salami Army <ultrakiff@gmail.com>
2017-05-09 21:36:51 +0200

    update keys

--- commands/getlarge.py
+++ commands/getlarge.py
@@ -4,7 +4,7 @@ from commands import get
 
 def run(bot, chat_id, user, keyConfig, message, totalResults=1):
     requestText = message.replace(bot.name, "").strip()
-    args = {'cx': keyConfig.get('Google', 'GCSE_SE_ID'),
+    args = {'cx': keyConfig.get('Google', 'GCSE_IMAGE_SE_ID1'),
             'key': keyConfig.get('Google', 'GCSE_APP_ID'),
             'searchType': "image",
             'safe': "off",
