a6551187f21b42868bc890c045557205685e3f34
Salami Army <ultrakiff@gmail.com>
2017-05-29 19:36:20 +0200

    add new alieses for /get and /getgif: /bekommen and /gifbekommen

---
+++ commands/bekommen.py
@@ -0,0 +1,3 @@
+import commands.get as get
+def run(bot, chat_id, user, keyConfig, message, totalResults=1):
+    get.run(bot, chat_id, user, keyConfig, message, totalResults)
---
+++ commands/gifbekommen.py
@@ -0,0 +1,3 @@
+import commands.getgif as getgif
+def run(bot, chat_id, user, keyConfig, message, totalResults=1):
+    getgif.run(bot, chat_id, user, keyConfig, message, totalResults)
