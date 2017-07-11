ee2defa5d8cab67684f696dc0162ee134ed20f69
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-07-11 08:09:13 +0200

    remastered login

--- commands/login.py
+++ commands/login.py
@@ -4,29 +4,52 @@ import uuid
 from google.appengine.ext import ndb
 import telegram
 
-watchedCommandName = 'login'
-
 class LoginCodeValue(ndb.Model):
     # key name: str(chat_id)
     currentValue = ndb.StringProperty(indexed=False, default='')
 
+class LoggedinValue(ndb.Model):
+    # key name: str(chat_id)
+    currentValue = ndb.BooleanProperty(indexed=False, default='')
+
 
 # ================================
 
+def Login(chat_id):
+    es = LoggedinValue.get_or_insert('loggedin:' + str(chat_id))
+    es.currentValue = True
+    es.put()
+
+def Logout(chat_id):
+    es = LoggedinValue.get_or_insert('loggedin:' + str(chat_id))
+    es.currentValue = False
+    es.put()
+
+def getLoggedinValue(chat_id):
+    es = LoggedinValue.get_by_id('loggedin:' + str(chat_id))
+    if es:
+        return es.currentValue
+    return False
+
 def setLoginCodeValue(chat_id, NewValue):
-    es = LoginCodeValue.get_or_insert(watchedCommandName + ':' + str(chat_id))
+    es = LoginCodeValue.get_or_insert('logincode:' + str(chat_id))
     es.currentValue = NewValue
     es.put()
 
 def getLoginCodeValue(chat_id):
-    es = LoginCodeValue.get_by_id(watchedCommandName + ':' + str(chat_id))
+    es = LoginCodeValue.get_by_id('logincode:' + str(chat_id))
     if es:
         return es.currentValue.encode('utf-8')
     return ''
 
 def run(bot, chat_id, user='Dave', keyConfig=None, message='', totalResults=1):
-    bot.sendMessage(chat_id=chat_id, text='Username: ' + str(chat_id) + '\nNew Password: ' + generate_new_pin(chat_id))
-
+    if message == '':
+        bot.sendMessage(chat_id=chat_id, text='Username: ' + str(chat_id))
+    elif message == getLoginCodeValue(chat_id):
+        Login(chat_id)
+        bot.sendMessage(chat_id=chat_id, text='Username: ' + str(chat_id) + '\nThat password is correct, you may proceed.')
+    #elif chat_id == keyConfig.get('BotAdministration', 'TESTING_TELEGRAM_PRIVATE_CHAT_ID') or chat_id == keyConfig.get('BotAdministration', 'TESTING_TELEGRAM_GROUP_CHAT_ID'):
+    #    bot.sendMessage(chat_id=chat_id, text='Username: ' + str(chat_id) + '\nYou are an admin!')
 
 def generate_new_pin(chat_id):
     loginCodeValue = str(uuid.uuid4())[:4]
