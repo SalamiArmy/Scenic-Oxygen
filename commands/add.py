22d914617b77ab10bf1e95abce2ede2fcca66f4a
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-10-04 11:01:17 +0200

    fix json parse error in /add

--- commands/add.py
+++ commands/add.py
@@ -15,14 +15,9 @@ def getTokenValue(repo_url):
 
 def run(bot, chat_id, user='Dave', keyConfig=None, message='', totalResults=1):
     request_text = str(message)
-    raw_data = urlfetch.fetch('https://api.github.com/repos/'+request_text.split(' ')[0]+'/'+request_text.split(' ')[1]+'/hooks',str(
-        {
-            "name": "web",
-            "active": "true",
-            "config":
-                {
-                    "url": "https://imageboet.com/github_webhook",
-                    "content_type": "json"
-                }
-}), 'POST',{'Authorization': 'token ' + request_text.split(' ')[2]})
+    raw_data = urlfetch.fetch('https://api.github.com/repos/' + request_text.split(' ')[0] +'/' + request_text.split(' ')[1] +'/hooks',
+                              "{\r\n  \"name\": \"web\",\r\n  \"active\": true,\r\n  \"config\": {\r\n    \"url\": \"" +
+                              keyConfig.get('InternetShortcut', 'URL') +
+                              "/github_webhook\",\r\n    \"content_type\": \"json\"\r\n  }\r\n}",
+                              urlfetch.POST, {'Authorization': 'token ' + request_text.split(' ')[2]})
     bot.sendMessage(chat_id=chat_id, text=raw_data.content)
---
+++ tests/test_add.py
@@ -0,0 +1,34 @@
+import ConfigParser
+import unittest
+import telegram
+
+import commands.add as add
+from google.appengine.ext import ndb
+from google.appengine.ext import testbed
+
+class TestLogin(unittest.TestCase):
+    def setUp(self):
+        # First, create an instance of the Testbed class.
+        self.testbed = testbed.Testbed()
+        # Then activate the testbed, which prepares the service stubs for use.
+        self.testbed.activate()
+        # Next, declare which service stubs you want to use.
+        self.testbed.init_datastore_v3_stub()
+        self.testbed.init_memcache_stub()
+        self.testbed.init_user_stub()
+        self.testbed.init_urlfetch_stub()
+        # Clear ndb's in-context cache between tests.
+        # This prevents data from leaking between tests.
+        # Alternatively, you could disable caching by
+        # using ndb.get_context().set_cache_policy(False)
+        ndb.get_context().clear_cache()
+
+    def test_login(self):
+        keyConfig = ConfigParser.ConfigParser()
+        keyConfig.read(["bot_keys.ini", "..\\bot_keys.ini"])
+        bot = telegram.Bot(keyConfig.get('BotIDs', 'TELEGRAM_BOT_ID'))
+        chatId = keyConfig.get('BotAdministration', 'TESTING_TELEGRAM_PRIVATE_CHAT_ID')
+
+        add.run(bot, chatId, 'Admin', keyConfig, keyConfig.get('BotAdministration', 'TESTING_GITHUB_USERNAME') + ' ' +
+                keyConfig.get('BotAdministration', 'TESTING_GITHUB_REPO') + ' ' +
+                keyConfig.get('BotAdministration', 'TESTING_GITHUB_TOKEN'))
