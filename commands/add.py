2352fd42b6dd1ca38c88f20f913fd4d8be67c8ba
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-10-09 09:32:05 +0200

    add extra logging here

--- commands/add.py
+++ commands/add.py
@@ -61,5 +61,6 @@ def create_hook(bot, chat_id, keyConfig, request_text):
         keyConfig.get('InternetShortcut', 'URL') +
         "/github_webhook\",\r\n    \"content_type\": \"json\"\r\n  }\r\n}",
         urlfetch.POST, {'Authorization': 'token ' + request_text.split(' ')[2]})
+    print 'Setting token value for ' + repo_url
     setTokenValue(repo_url, request_text.split(' ')[0])
     bot.sendMessage(chat_id=chat_id, text=raw_data.content)
