b2129029e23c0c87561e1bae38c6a7d7a3a742d1
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-10-27 09:56:04 +0200

    mention /login pin is hidden from preview

--- commands/login.py
+++ commands/login.py
@@ -51,5 +51,6 @@ def generate_new_pin(chat_id):
 def run(bot, chat_id, user='Dave', keyConfig=None, message='', totalResults=1):
     str_chat_id = str(chat_id)
     pin = generate_new_pin(str_chat_id)
-    bot.sendMessage(chat_id=chat_id, text=user + ', new One Time Pin is:\n\n\n\n\n\n\n' + pin)
+    bot.sendMessage(chat_id=chat_id, text=user + ', your new One Time Pin' +
+                                          ' is hidden from preview below:\n\n\n\n\n\n\n' + pin)
     return 'New One Time Pin sent to ' + str_chat_id + ' for ' + user
