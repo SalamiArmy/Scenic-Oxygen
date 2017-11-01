5611678bb8c918d4e2c966aeb3cf455e8b137f43
Ashley Lewis <ashley.lewis@dev2.co.za>
2017-11-01 09:55:52 +0200

    mention module name in dynamic module loading log

--- main.py
+++ main.py
@@ -291,9 +291,9 @@ def load_code_as_module(module_name):
             if command_code != '':
                 module = sys.modules.setdefault(module_name, imp.new_module(module_name))
                 try:
-                    logging.info('begin exec')
+                    logging.info('begin loading module ' + module_name)
                     exec command_code in module.__dict__
-                    logging.info('end exec')
+                    logging.info('end loading module ' + module_name)
                 except ImportError:
                     print module_name + '\n' + \
                           'imports between commands must be replaced with command = main.load_code_as_module(command) ' + \
