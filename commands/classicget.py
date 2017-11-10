9465cf07584d2a69f0455f27b91ee67532330db8
Salami Army <ultrakiff@gmail.com>
2017-11-10 10:25:22 +0200

    move app engine project out into a config file

--- commands/classicget.py
+++ commands/classicget.py
@@ -2,6 +2,9 @@ from google.cloud import logging
 from google.auth import app_engine
 
 def run(bot, chat_id, user='Dave', keyConfig=None, message='', totalResults=1):
-    client = logging.Client(project='scenic-oxygen-113812', credentials=app_engine.Credentials())
+    client = logging.Client(
+        project=keyConfig.get('Logging', 'APP_ENGINE_PROJECT_KEY'),
+        credentials=app_engine.Credentials()
+    )
     list_entries = client.list_entries()
     bot.sendMessage(chat_id=chat_id, text=list_entries.num_results)
